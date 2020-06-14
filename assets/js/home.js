6f4c4fe0f9f6226a86ad57ab509f61ba277c1968
shasingh <sharandeep.singh@niceactimize.com>
2020-06-14 18:06:56 +0530

    working templates and tags

--- assets/js/home.js
+++ assets/js/home.js
@@ -1,55 +1,104 @@
 $(document).ready(function(){
     getTags();
-    getTemplates();
-})
+    onTagChangeHandler();
+});
+let tagPage = 1;
+let templatePage = 1;
+let radioEleStr = 'input[type="radio"][name="tagRadios"]';
+
+function getTemplatesOnPageLoad(){
+    $("#tagsList").find(radioEleStr).first().trigger("click");
+
+}
+function getTagsApi(){
+    return new Promise((resolve,reject)=>{
+        ajax.get("api/tags.php",{page:tagPage,count:25})
+        .then((response)=>{
+            resolve(response);
+        })
+        .catch((error)=>{
+            alert("Some error occured");
+            console.error(error);
+            reject(error);
+        });
+    })
+}
 
 function getTags(){
-    ajax.get("api/tags.php",{page:1,count:25})
-    .then((response)=>{
-        let tagsListEle = $("#tagsList")
-        if(response && Array.isArray(response.data)){
-            let notChecked = true;
-            let tagsArr = response.data.map((tag)=>{
-                let tagTemplate = $(tagsListEle.find("template").html());
-                let tagId = `tag${tag.id}`;
-                let tagRadio = tagTemplate.find('[type="radio"]');
-                if(notChecked){
-                    tagRadio.attr("checked",true);
-                    notChecked = false;
-                }
-                tagRadio.attr("id",tagId)
-                tagRadio.attr("value",tag.tagName);
-                let tagLabel = tagTemplate.find("label");
-                tagLabel.attr("for",tagId);
-                tagLabel.text(tag.tagName);
-                return tagTemplate;
-
-            });
-            tagsListEle.append(tagsArr);
-        }
-    })
-    .catch((error)=>{
-        alert("Some error occured");
-        console.error(error);
+    $("#tagsList").find(radioEleStr).not("template").remove();
+    getTagsApi().then((response)=>{
+        drawTagsUI(response);
+        getTemplatesOnPageLoad();
     });
 }
 
-function getTemplates(){
-    ajax.get("api/templates.php",{page:1,count:25,tagName:"Poaceae"})
-    .then((response)=>{
-        let templatesListEle = $("#templatesList")
-        if(response && Array.isArray(response.data)){
-            let templatesArr = response.data.map((tag)=>{
-                let template = $(templatesListEle.find("template").html());
-                let html = `<i class="fa fa-tag"></i> ${tag.tagName}`;
-                template.find('[name="tag"]').html(html)
-                return template;
+function getMoreTags(){
+    getTagsApi().then((response)=>{drawTagsUI(response)})
+}
 
-            });
-            templatesListEle.append(templatesArr);
-        }
+function drawTagsUI(response){
+    let tagsListEle = $("#tagsList");
+    if(response && Array.isArray(response.data)){
+        let tagsArr = response.data.map((tag)=>{
+            let tagTemplate = $(tagsListEle.find("template").html());
+            let tagId = `tag${tag.id}`;
+            let tagRadio = tagTemplate.find('[type="radio"]');
+            tagRadio.attr("id",tagId)
+            tagRadio.attr("value",tag.tagName);
+            let tagLabel = tagTemplate.find("label");
+            tagLabel.attr("for",tagId);
+            tagLabel.text(tag.tagName);
+            return tagTemplate;
+
+        });
+        tagsListEle.append(tagsArr);
+        tagPage++;
+    }
+}
+
+function getTemplatesApi(tagName){
+    return new Promise((resolve,reject)=>{
+        ajax.get("api/templates.php",{page:1,count:25,tagName:tagName})
+        .then((response)=>{
+            resolve(response);
+        })
+        .catch((error)=>{
+            alert("Something went wrong");
+            console.error(error);
+            reject(error);
+        });
     })
-    .catch((error)=>{
-        console.error(error);
-    });
+}
+
+function getTemplates(tagName){
+    $("#templatesList").find('[name="tagTemplates"]').not("template").remove();
+    getTemplatesApi(tagName).then((response)=>{
+        drawTemplatesUI(response);
+    })
+}
+
+function getMoreTemplates(tagName){
+    getTemplatesApi(tagName).then((response)=>{
+        drawTemplatesUI(response);
+    })
+}
+
+function drawTemplatesUI(response){
+    let templatesListEle = $("#templatesList")
+    if(response && Array.isArray(response.data)){
+        let templatesArr = response.data.map((tag)=>{
+            let template = $(templatesListEle.find("template").html());
+            let html = `<i class="fa fa-tag"></i> ${tag.tagName}`;
+            template.find('[name="tag"]').html(html)
+            return template;
+
+        });
+        templatesListEle.append(templatesArr);
+    }
+}
+
+function onTagChangeHandler(){
+    $("#tagsList").on("change",radioEleStr,function(){
+        getTemplates(this.value);
+    })
 }
--- shared/templates-view.php
+++ shared/templates-view.php
@@ -3,7 +3,7 @@
 <div class="parent-flex container-fluid">
     <div class="row flex-grow-overflow" id="templatesList">
         <template>
-            <div class="col-lg-4 col-md-6 col-12 p-0">
+            <div class="col-lg-4 col-md-6 col-12 p-0" name="tagTemplates">
                 <div class="card m-2">
                     <img class="card-img-top border" src="assets/images/green-leaves-top.png" alt="Card image">
                     <div class="card-body">
