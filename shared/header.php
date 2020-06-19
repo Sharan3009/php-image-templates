a8b48880693aed261c6b78a8738695118539f490
shasingh <sharandeep.singh@niceactimize.com>
2020-06-20 02:03:31 +0530

    forgot to commit header

--- shared/header.php
+++ shared/header.php
@@ -1,3 +1,3 @@
 <nav class="navbar navbar-dark bg-dark">
-  <span class="navbar-brand" style="cursor:pointer;" id="toggleToDesigns"><?php echo $GLOBALS['configToggleToDesignBtnText']?></span>
+  <span class="navbar-brand" style="cursor:pointer;" id="toggleToDesigns"><?php echo $config_toggle_to_design_btn_text?></span>
 </nav>
