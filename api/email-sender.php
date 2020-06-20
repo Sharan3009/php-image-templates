b607f7fbac6c9941022943bb67581a2884526f96
shasingh <sharandeep.singh@niceactimize.com>
2020-06-20 11:15:11 +0530

    minor change email-sender

--- api/email-sender.php
+++ api/email-sender.php
@@ -20,36 +20,36 @@
         $fileUrl = $obj['fileUrl'];
         $emailSubject = "Templates";
         $emailBody = "<!DOCTYPE html>
-            <html>
-                <head>
-                    <meta http-equiv='Content-Type' content='text/html; charset=utf-8' />
-                </head>
-                <body>
-                    $fileUrl
-                </body>
-            </html>";
+                        <html>
+                            <head>
+                                <meta http-equiv='Content-Type' content='text/html; charset=utf-8' />
+                            </head>
+                            <body>
+                                $fileUrl
+                            </body>
+                        </html>";
 
-            $mail = new PHPMailer();
-            $mail->IsSMTP();
-            $mail->SMTPDebug = 0;
-            $mail->SMTPAuth = TRUE;
-            $mail->SMTPSecure = "tls";
-            $mail->Port     = 587;  
-            $mail->Username = "gochat30091994@gmail.com";
-            $mail->Password = "Password@94";
-            $mail->Host     = "smtp.gmail.com";
-            $mail->Mailer   = "smtp";
-            $mail->SetFrom("test@testdomain.com", "Template Generator");
-            $mail->AddAddress($email);
-            $mail->Subject = $emailSubject;
-            $mail->WordWrap   = 80;
-            $content = $emailBody;
-            $mail->MsgHTML($content);
-            $mail->IsHTML(true);
-            if(!$mail->Send()) 
-            echo json_encode(false);
-            else 
-            echo json_encode(true);
+        $mail = new PHPMailer();
+        $mail->IsSMTP();
+        $mail->SMTPDebug = 0;
+        $mail->SMTPAuth = TRUE;
+        $mail->SMTPSecure = "tls";
+        $mail->Port     = 587;  
+        $mail->Username = "gochat30091994@gmail.com";
+        $mail->Password = "Password@94";
+        $mail->Host     = "smtp.gmail.com";
+        $mail->Mailer   = "smtp";
+        $mail->SetFrom("test@testdomain.com", "Template Generator");
+        $mail->AddAddress($email);
+        $mail->Subject = $emailSubject;
+        $mail->WordWrap   = 80;
+        $content = $emailBody;
+        $mail->MsgHTML($content);
+        $mail->IsHTML(true);
+        if(!$mail->Send()) 
+        echo json_encode(false);
+        else 
+        echo json_encode(true);
     };
 
 ?>
