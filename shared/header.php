dc5f36eb25189ede325a71ad0966ac054d9ee3d7
shasingh <sharandeep.singh@niceactimize.com>
2020-06-28 19:22:29 +0530

    renamed php's

--- shared/header.php
+++ shared/header.php
@@ -12,17 +12,17 @@
       <div class="container">
         <div class="row align-items-center">
           <div class="col-11 col-xl-2">
-            <h1 class="mb-0 site-logo"><a href="landing.php"
+            <h1 class="mb-0 site-logo"><a href="home.php"
                 class="text-black h2 mb-0">Create<span class="text-primary">.</span> </a></h1>
           </div>
           <div class="col-12 col-md-10 d-none d-xl-block">
             <nav class="site-navigation position-relative text-right" role="navigation">
               <ul class="site-menu main-menu js-clone-nav mr-auto d-none d-lg-block">
-                <li><a href="home.php" class="nav-link"><?php echo $config_toggle_to_design_btn_text?></a></li>
-                <li><a href="landing.php#how_to_design" class="nav-link"><?php echo "How To Design" ?></a></li>
-                <li><a href="landing.php#faq" class="nav-link"><?php echo "FAQ" ?></a></li>
-                <li><a href="landing.php#about-section" class="nav-link"><?php echo "About Us" ?></a></li>
-                <li><a href="landing.php#contact-section" class="nav-link"><?php echo "Contact Us" ?></a></li>
+                <li><a href="templates.php" class="nav-link"><?php echo $config_toggle_to_design_btn_text?></a></li>
+                <li><a href="home.php#how_to_design" class="nav-link"><?php echo "How To Design" ?></a></li>
+                <li><a href="home.php#faq" class="nav-link"><?php echo "FAQ" ?></a></li>
+                <li><a href="home.php#about-section" class="nav-link"><?php echo "About Us" ?></a></li>
+                <li><a href="home.php#contact-section" class="nav-link"><?php echo "Contact Us" ?></a></li>
               </ul>
             </nav>
           </div>
---
+++ templates.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+    <?php include "shared/minimal-css.php" ?>
+    <link href="Create%20%E2%80%94%20Onepage%20Website%20Template%20by%20Colorlib_files/css.css" rel="stylesheet">
+    <link rel="stylesheet" href="Create%20%E2%80%94%20Onepage%20Website%20Template%20by%20Colorlib_files/style.css">
+    <link rel="stylesheet" href="Create%20%E2%80%94%20Onepage%20Website%20Template%20by%20Colorlib_files/bootstrap.css">
+    <link rel="stylesheet" href="Create%20%E2%80%94%20Onepage%20Website%20Template%20by%20Colorlib_files/jquery-ui.css">
+    <link rel="stylesheet" href="Create%20%E2%80%94%20Onepage%20Website%20Template%20by%20Colorlib_files/owl_002.css">
+    <link rel="stylesheet" href="Create%20%E2%80%94%20Onepage%20Website%20Template%20by%20Colorlib_files/owl.css">
+    <link rel="stylesheet" href="Create%20%E2%80%94%20Onepage%20Website%20Template%20by%20Colorlib_files/owl.css">
+    <link rel="stylesheet" href="Create%20%E2%80%94%20Onepage%20Website%20Template%20by%20Colorlib_files/jquery.css">
+    <link rel="stylesheet"
+        href="Create%20%E2%80%94%20Onepage%20Website%20Template%20by%20Colorlib_files/bootstrap-datepicker.css">
+    <link rel="stylesheet" href="Create%20%E2%80%94%20Onepage%20Website%20Template%20by%20Colorlib_files/flaticon.css">
+    <link rel="stylesheet" href="Create%20%E2%80%94%20Onepage%20Website%20Template%20by%20Colorlib_files/aos.css">
+    <link rel="stylesheet" href="Create%20%E2%80%94%20Onepage%20Website%20Template%20by%20Colorlib_files/style_002.css">
+    <!-- <link href="assets/css/fontselect-alternate.css" rel="stylesheet" type="text/css" media="all" /> -->
+    <link href="assets/css/home.css" rel="stylesheet" type="text/css" media="all" />
+</head>
+<body>
+    <!-- this apply-font element will stay everytime in order to preload the selected font -->
+    <div class="position-fixed apply-font" style="top:-1000px">preload font</div>
+    <?php include "config/config.php" ?>
+    <?php include "shared/header.php" ?>
+
+    <div class="container-fluid parent-flex" id="selection">
+        <div class="row">
+            <div class="col-md-3 d-md-flex d-none parent-flex border-right px-0">
+                <?php include "shared/tags-view.php" ?>
+            </div>
+            <div class="col-md-9 parent-flex px-0">
+                <?php include "shared/templates-view.php" ?>
+            </div>
+        </div>
+    </div>
+
+    <div id="generatePdf" class="parent-flex d-none">
+        <?php include "shared/template-generate.php" ?>
+    </div>
+
+    <?php include "shared/minimal-js.php" ?>
+    <?php include "shared/html-template-tags.php" ?>
+    <script src="assets/js/fontpicker.js" type="text/javascript"></script>
+    <script>
+        configMaxLengthPerName = <?php echo $config_max_length_per_name ?>;
+        configMaxPdfPages = <?php echo $config_max_pdf_pages ?>;
+        configInitialPageNumber = <?php echo $config_initial_page_number ?>;
+        configCountPerPage = <?php echo $config_count_per_page ?>;
+        configToggleToDesignBtnText = "<?php echo $config_toggle_to_design_btn_text ?>";
+    </script>
+    <script src="assets/js/home.js" type="text/javascript"></script>
+</body>
+</html>
