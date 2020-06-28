f0f84a5bbc273dea9b90330193e06ab07dff9887
shasingh <sharandeep.singh@niceactimize.com>
2020-06-29 03:12:43 +0530

    changed title

--- home.php
+++ home.php
@@ -3,7 +3,7 @@
 
 <head>
   <meta http-equiv="content-type" content="text/html; charset=UTF-8">
-  <title>Create — Onepage Website Template by Colorlib</title>
+  <title>PlaceCards | Home</title>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
   <?php include "shared/minimal-css.php" ?>
--- templates.php
+++ templates.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Home</title>
+    <title>PlaceCards | Templates</title>
     <?php include "shared/minimal-css.php" ?>
     <link href="assets/css/home.css" rel="stylesheet" type="text/css" media="all" />
 </head>
