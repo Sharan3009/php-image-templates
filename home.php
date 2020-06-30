f73f88f69b5b3f9da4f06332c30b2cec06eeaf54
shasingh <sharandeep.singh@niceactimize.com>
2020-06-30 22:40:23 +0530

    copyright

--- home.php
+++ home.php
@@ -216,7 +216,7 @@
             <div class="border-top pt-2">
               <p>
                 Copyright ©
-                <script type="text/javascript">document.write(new Date().getFullYear());</script>2020 All rights
+                <script type="text/javascript">document.write(new Date().getFullYear());</script> All rights
                 reserved | This template is made with <i class="fa fa-heart text-danger" aria-hidden="true"></i> by <a
                   href="https://colorlib.com/" target="_blank">Colorlib</a>
               </p>
