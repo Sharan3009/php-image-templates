553141adde05f809fda10b8e8f5d8a05aad832a6
shasingh <sharandeep.singh@niceactimize.com>
2020-07-11 15:05:47 +0530

    config and changed font size

--- shared/template-preview.php
+++ shared/template-preview.php
@@ -1,7 +1,7 @@
 <div class="selected-template">
-    <div class="template-name-text apply-font w-100 text-center" style="font-size:6vw;" id="placecardNameText">
+    <div class="template-name-text apply-font w-100 text-center" style="font-size:5vw;" id="placecardNameText">
     </div>
-    <div class="template-table-text apply-font w-100 text-center" style="font-size:3vw;" id="placecardTableText">
+    <div class="template-table-text apply-font w-100 text-center" style="font-size:4vw;" id="placecardTableText">
         Table One
     </div>
 </div>
